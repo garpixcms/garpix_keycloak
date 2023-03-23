006c61cfd2488f1804babd1017d99a379da2a753
griviala <78097561+griviala@users.noreply.github.com>
2023-03-23 09:51:25 +0300

    1.2.1

--- backend/garpix_keycloak/backends.py
+++ backend/garpix_keycloak/backends.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import get_user_model
+
 from garpix_keycloak.services import KeycloakService
 
 
@@ -6,4 +8,10 @@ class KeycloakAuthenticationBackend:
         try:
             return KeycloakService().get_user_from_request(request)
         except Exception:
+            return None
+
+    def get_user(self, user_id):
+        try:
+            return get_user_model().active_objects.get(pk=user_id)
+        except get_user_model().DoesNotExist:
             return None
